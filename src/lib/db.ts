d660a4eb2dfa9e1b45cfcdd032a353a47bc8b2a4
Mubashir Ali <alimubashir822@gmail.com>
2026-06-18 03:35:09 +0500

    chore: enable remote database configs and push seeded dev.db database file to fix vercel runtime connection error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,11 +3,15 @@ import { PrismaLibSql } from '@prisma/adapter-libsql';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const url = process.env.TURSO_DATABASE_URL || 'file:dev.db';
+const authToken = process.env.TURSO_AUTH_TOKEN;
+
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     adapter: new PrismaLibSql({
-      url: 'file:dev.db'
+      url,
+      authToken
     })
   });
 
